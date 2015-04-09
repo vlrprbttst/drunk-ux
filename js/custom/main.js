c73b2dee89260f76009725413bcaba654f5bb930
Valerio Pierbattista <valeriopierbattista@yahoo.it>
2015-04-09 17:39:37 +0200

    new alert added
    new alert added

--- js/custom/main.js
+++ js/custom/main.js
@@ -27,29 +27,52 @@ $(".drunk").click(function() {
 
 });
 
-if ($('.center input').val() != 'http://') {
+if ($('#url').val() != 'http://') {
 	$(".center").addClass("url-active");
 }
 
-$( ".center input" ).focus(function() {
- $(".center").addClass("url-active");
+$("#url").focus(function() {
+	$(".center").addClass("url-active");
 });
 
-$(".alert-url").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){ 
-	$(".alert-url").css("display","none");
-	
-	});
+$(".alert-url").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
+	$(".alert-url").css("display", "none");
 
+});
 
 if (window.location.href.indexOf("http%") > -1) {
-    $(".right").addClass("godrunk");
-    
+	$(".right").addClass("godrunk");
+
 }
+
+setInterval(function(){ 
+    
+    var forbiddenSites = ['google','facebook','linkedin'],
+        containWord = false,
+        valInput = $('#url').val();
+    
+    for(var i = 0;i<forbiddenSites.length;i++){
+        if(valInput.indexOf(forbiddenSites[i]) != -1){
+            containWord = true;
+        }
+    }    
+if (containWord) {
+	$('body').addClass('forbidden');
+}
+    
+    else {
+    $('body').removeClass('forbidden');
+    }
+    
+
+}, 500);
 
 // twitter button
 
 window.twttr = ( function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0], t = window.twttr || {};
+		var js,
+		    fjs = d.getElementsByTagName(s)[0],
+		    t = window.twttr || {};
 		if (d.getElementById(id))
 			return;
 		js = d.createElement(s);
@@ -71,7 +94,8 @@ window.twttr = ( function(d, s, id) {
 	function() {
 		(i[r].q = i[r].q || []).push(arguments)
 	}, i[r].l = 1 * new Date();
-	a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+	a = s.createElement(o),
+	m = s.getElementsByTagName(o)[0];
 	a.async = 1;
 	a.src = g;
 	m.parentNode.insertBefore(a, m)
